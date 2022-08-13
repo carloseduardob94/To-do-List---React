f5d39f7fc536916eac6d933a92c738374f55e007
Eduardo <educonts08@gmail.com>
2022-08-13 14:27:20 -0300

    types paste created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import { Container } from './App.styles'
+import {useState} from 'react'
+import { Container, Area, Header } from './App.styles'
+import { Item } from './types/Item';
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,
+      name: 'Comprar o pão na padaria',
+      done: false
+    },
+    {
+      id: 2,
+      name: 'Comprar o bolo na padaria',
+      done: false
+    }
+  ])
+
+
   return(
     <Container>
-      
+      <Area>
+        <Header>Lista de Tarefas</Header>
+        {list.map((item, index)=>(
+          <div>...</div>
+        ))}
+      </Area>
     </Container>
   )
 }
